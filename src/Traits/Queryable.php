022e12395e73d672a87f880c989f39d7159adad0
Phạm Quang Hiếu <hieu.pham@cloudteam.vn>
2020-12-29 17:10:27 +0700

    refactor: enhance dateBetween

--- src/Traits/Queryable.php
+++ src/Traits/Queryable.php
@@ -7,141 +7,148 @@ use Illuminate\Support\Carbon;
 
 trait Queryable
 {
-    use Filterable;
+	use Filterable;
 
-    /**
-     * @param Builder $query
-     * @param array $conditions
-     *
-     * @return Builder
-     */
-    public function scopeAndFilterWhere($query, $conditions)
-    {
-        if (blank($conditions)) {
-            return $query;
-        }
+	/**
+	 * @param Builder $query
+	 * @param array $conditions
+	 *
+	 * @return Builder
+	 */
+	public function scopeAndFilterWhere($query, $conditions)
+	{
+		if (blank($conditions)) {
+			return $query;
+		}
 
-        if (is_array($conditions[0])) {
-            foreach ($conditions as $condition) {
-                $this->addCondition($condition);
-            }
+		if (is_array($conditions[0])) {
+			foreach ($conditions as $condition) {
+				$this->addCondition($condition);
+			}
 
-            return $this->build($query);
-        }
-        $this->addCondition($conditions);
+			return $this->build($query);
+		}
+		$this->addCondition($conditions);
 
-        return $this->build($query);
-    }
+		return $this->build($query);
+	}
 
-    /**
-     * @param Builder $query
-     * @param array $dates
-     * @param string $column
-     * @param string $format
-     * @param string $boolean
-     * @param bool $not
-     *
-     * @return Builder
-     */
-    public function scopeDateBetween($query, $dates, $column = 'created_at', $format = 'd-m-Y', $boolean = 'and', $not = false)
-    {
-        [$fromDate, $toDate] = $dates;
-        if (blank($fromDate) || blank($toDate)) {
-            return $query;
-        }
+	/**
+	 * @param Builder $query
+	 * @param array $dates
+	 * @param string $column
+	 * @param string $format
+	 * @param string $boolean
+	 * @param bool $not
+	 *
+	 * @return Builder
+	 */
+	public function scopeDateBetween($query, $dates, $column = 'created_at', $format = 'd-m-Y', $boolean = 'and', $not = false)
+	{
+		[$fromDate, $toDate] = $dates;
 
-        $fromDate = Carbon::createFromFormat($format, $fromDate);
-        $toDate   = Carbon::createFromFormat($format, $toDate);
+		if (blank($fromDate) && blank($toDate)) {
+			return $query;
+		}
 
-        return $query->whereBetween($column, [
-            $fromDate->toDateString() . ' 00:00:00',
-            $toDate->toDateString() . ' 23:59:59',
-        ], $boolean, $not);
-    }
+		if (blank($fromDate) && $toDate) {
+			$fromDate = $toDate;
+		} elseif (blank($toDate) && $fromDate) {
+			$toDate = $fromDate;
+		}
 
-    /**
-     * Query theo điều kiện giá trị field không thuộc khoảng từ start => end
-     *
-     * @param Builder $query
-     * @param array|string $excludes
-     * @param string $field : Tên field muốn query
-     *
-     * @return Builder|\Illuminate\Database\Query\Builder
-     */
-    public function scopeExclude(Builder $query, $excludes, $field = 'id')
-    {
-        $excludes = is_array($excludes) ? $excludes : explode(',', $excludes);
+		$fromDate = Carbon::createFromFormat($format, $fromDate);
+		$toDate   = Carbon::createFromFormat($format, $toDate);
 
-        return $excludes ? $query->whereNotIn($field, $excludes) : $query;
-    }
+		return $query->whereBetween($column, [
+			$fromDate->toDateString() . ' 00:00:00',
+			$toDate->toDateString() . ' 23:59:59',
+		], $boolean, $not);
+	}
 
-    /**
-     * @param $query
-     * @param array $filterDatas   Dữ liệu dùng để filter
-     * @param string $boolean
-     * @param array $filterConfigs Custom filter config
-     *
-     * @return mixed
-     */
-    public function scopeFilters($query, $filterDatas, $boolean = 'and', array $filterConfigs = null)
-    {
-        if ( ! property_exists($this, 'filters') || blank($filterDatas)) {
-            return $query;
-        }
+	/**
+	 * Query theo điều kiện giá trị field không thuộc khoảng từ start => end
+	 *
+	 * @param Builder $query
+	 * @param array|string $excludes
+	 * @param string $field : Tên field muốn query
+	 *
+	 * @return Builder|\Illuminate\Database\Query\Builder
+	 */
+	public function scopeExclude(Builder $query, $excludes, $field = 'id')
+	{
+		$excludes = is_array($excludes) ? $excludes : explode(',', $excludes);
 
-        //property $filters của model
-        $filters = $this->filters;
-        if ($filterConfigs) {
-            $filters = array_merge($filters, $filterConfigs);
-        }
+		return $excludes ? $query->whereNotIn($field, $excludes) : $query;
+	}
 
-        foreach ($filters as $column => $operator) {
-            if (isset($filterDatas[$column]) || is_array($operator)) {
-                $filterVal = $filterDatas[$column] ?? '';
-                $this->addCondition([$column, $operator, $filterVal], $boolean, $filterDatas);
-            }
-        }
+	/**
+	 * @param $query
+	 * @param array $filterDatas   Dữ liệu dùng để filter
+	 * @param string $boolean
+	 * @param array $filterConfigs Custom filter config
+	 *
+	 * @return mixed
+	 */
+	public function scopeFilters($query, $filterDatas, $boolean = 'and', array $filterConfigs = null)
+	{
+		if ( ! property_exists($this, 'filters') || blank($filterDatas)) {
+			return $query;
+		}
 
-        return $this->build($query);
-    }
+		//property $filters của model
+		$filters = $this->filters;
+		if ($filterConfigs) {
+			$filters = array_merge($filters, $filterConfigs);
+		}
 
-    /**
-     * Query theo điều kiện giá trị field thuộc khoảng từ start => end
-     *
-     * @param Builder $query
-     * @param array|string $includes
-     * @param string $field : Tên field muốn query
-     *
-     * @return Builder|\Illuminate\Database\Query\Builder
-     */
-    public function scopeInclude(Builder $query, $includes, $field = 'id')
-    {
-        $includes = is_array($includes) ? $includes : explode(',', $includes);
+		foreach ($filters as $column => $operator) {
+			if (isset($filterDatas[$column]) || is_array($operator)) {
+				$filterVal = $filterDatas[$column] ?? '';
+				$this->addCondition([$column, $operator, $filterVal], $boolean, $filterDatas);
+			}
+		}
 
-        return $includes ? $query->whereIn($field, $includes) : $query;
-    }
+		return $this->build($query);
+	}
 
-    /**
-     * @param Builder $query
-     * @param array $conditions
-     *
-     * @return Builder
-     */
-    public function scopeOrFilterWhere($query, $conditions)
-    {
-        if (blank($conditions)) {
-            return $query;
-        }
+	/**
+	 * Query theo điều kiện giá trị field thuộc khoảng từ start => end
+	 *
+	 * @param Builder $query
+	 * @param array|string $includes
+	 * @param string $field : Tên field muốn query
+	 *
+	 * @return Builder|\Illuminate\Database\Query\Builder
+	 */
+	public function scopeInclude(Builder $query, $includes, $field = 'id')
+	{
+		$includes = is_array($includes) ? $includes : explode(',', $includes);
 
-        if (is_array($conditions[0])) {
-            foreach ($conditions as $condition) {
-                $this->addCondition($condition, 'or');
-            }
+		return $includes ? $query->whereIn($field, $includes) : $query;
+	}
 
-            return $this->build($query);
-        }
-        $this->addCondition($conditions, 'or');
+	/**
+	 * @param Builder $query
+	 * @param array $conditions
+	 *
+	 * @return Builder
+	 */
+	public function scopeOrFilterWhere($query, $conditions)
+	{
+		if (blank($conditions)) {
+			return $query;
+		}
 
-        return $this->build($query);
-    }
+		if (is_array($conditions[0])) {
+			foreach ($conditions as $condition) {
+				$this->addCondition($condition, 'or');
+			}
+
+			return $this->build($query);
+		}
+		$this->addCondition($conditions, 'or');
+
+		return $this->build($query);
+	}
 }
