710fbadd31bf52d9e8762cbe4072f68cafc70668
Phạm Quang Hiếu <hieu.pham@cloudteam.vn>
2021-06-30 10:58:39 +0700

    refactor: update mô tả của activity_log

--- src/Traits/Modelable.php
+++ src/Traits/Modelable.php
@@ -6,53 +6,53 @@ use Illuminate\Support\Str;
 
 trait Modelable
 {
-    public function getTableNameSingularAttribute(): string
-    {
-        return Str::singular($this->getTable());
-    }
+	public function getTableNameSingularAttribute(): string
+	{
+		return Str::singular($this->getTable());
+	}
 
-    public function getCanBeCreatedAttribute()
-    {
-        $name = $this->table_name_singular;
+	public function getCanBeCreatedAttribute()
+	{
+		$name = $this->table_name_singular;
 
-        try {
-            return can("create_$name");
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
+		try {
+			return can("create_$name");
+		} catch (\Exception $e) {
+			return false;
+		}
+	}
 
-    public function getCanBeEditedAttribute()
-    {
-        $name = $this->table_name_singular;
+	public function getCanBeEditedAttribute()
+	{
+		$name = $this->table_name_singular;
 
-        try {
-            return can("edit_$name");
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
+		try {
+			return can("edit_$name");
+		} catch (\Exception $e) {
+			return false;
+		}
+	}
 
-    public function getCanBeDeletedAttribute()
-    {
-        $name = $this->table_name_singular;
+	public function getCanBeDeletedAttribute()
+	{
+		$name = $this->table_name_singular;
 
-        try {
-            return can("delete_$name");
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
+		try {
+			return can("delete_$name");
+		} catch (\Exception $e) {
+			return false;
+		}
+	}
 
-    public function getCreatedAtTextAttribute()
-    {
-        return $this->created_at->format(config('basecore.datetime_format', 'd-m-Y H:i:s'));
-    }
+	public function getCreatedAtTextAttribute()
+	{
+		return $this->created_at->format(config('basecore.datetime_format', 'd-m-Y H:i:s'));
+	}
 
-    public function getUpdatedAtTextAttribute()
-    {
-        return optional($this->updated_at)->format(config('basecore.datetime_format', 'd-m-Y H:i:s'));
-    }
+	public function getUpdatedAtTextAttribute()
+	{
+		return optional($this->updated_at)->format(config('basecore.datetime_format', 'd-m-Y H:i:s'));
+	}
 
 	public function getDescriptionEvent(string $eventName): string
 	{
@@ -63,9 +63,13 @@ trait Modelable
 		}
 		$user     = auth()->user();
 		$username = $user->username ?? 'System';
-		$dateTime = now()->toDateTimeString();
+		$dateTime = now()->format('d-m-Y H:i:s');
 		$ip       = request()->getClientIp();
 
-		return sprintf('%s %s%s %s %s %s', $this->classLabel(), $displayText, __(" has been {$eventName} by "), $username, " vào lúc $dateTime từ địa chỉ IP $ip.", $this->logMessage);
+		if ($this->logMessage) {
+			return sprintf('%s %s%s %s %s %s', $this->classLabel(), $displayText, __(" has been {$eventName} by "), $username, " vào lúc $dateTime từ địa chỉ IP $ip. Chi tiết:", $this->logMessage);
+		}
+
+		return sprintf('%s %s%s %s %s', $this->classLabel(), $displayText, __(" has been {$eventName} by "), $username, " vào lúc $dateTime từ địa chỉ IP $ip.");
 	}
 }
