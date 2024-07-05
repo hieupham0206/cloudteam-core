a3255cb55784438f8c5547c69f327852a90ee867
Hieu Pham <hieu.pham@cloudteam.vn>
2024-07-05 16:24:30 +0700

    refactor: check table exists in migration file

--- database/migrations/2018_10_02_093643_create_permission_tables.php
+++ database/migrations/2018_10_02_093643_create_permission_tables.php
@@ -13,75 +13,78 @@ class CreatePermissionTables extends Migration
      */
     public function up()
     {
+
         $tableNames = config('permission.table_names');
         $columnNames = config('permission.column_names');
 
-        Schema::create($tableNames['permissions'], function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('guard_name')->default('web');
-            $table->string( 'module' )->default( '' );
-            $table->string( 'action' )->default( '' );
-            $table->timestamps();
-        });
+        if (! Schema::hasTable($tableNames['permissions'])) {
+            Schema::create($tableNames['permissions'], function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('name');
+                $table->string('guard_name')->default('web');
+                $table->string( 'module' )->default( '' );
+                $table->string( 'action' )->default( '' );
+                $table->timestamps();
+            });
 
-        Schema::create($tableNames['roles'], function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('guard_name')->default('web');
-            $table->timestamps();
-        });
+            Schema::create($tableNames['roles'], function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('name');
+                $table->string('guard_name')->default('web');
+                $table->timestamps();
+            });
 
-        Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames) {
-            $table->unsignedBigInteger('permission_id');
+            Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames) {
+                $table->unsignedBigInteger('permission_id');
 
-            $table->string('model_type');
-            $table->unsignedBigInteger($columnNames['model_morph_key']);
-            $table->index([$columnNames['model_morph_key'], 'model_type', ]);
+                $table->string('model_type');
+                $table->unsignedBigInteger($columnNames['model_morph_key']);
+                $table->index([$columnNames['model_morph_key'], 'model_type', ]);
 
-            $table->foreign('permission_id')
-                ->references('id')
-                ->on($tableNames['permissions'])
-                ->onDelete('cascade');
+                $table->foreign('permission_id')
+                      ->references('id')
+                      ->on($tableNames['permissions'])
+                      ->onDelete('cascade');
 
-            $table->primary(['permission_id', $columnNames['model_morph_key'], 'model_type'],
+                $table->primary(['permission_id', $columnNames['model_morph_key'], 'model_type'],
                     'model_has_permissions_permission_model_type_primary');
-        });
+            });
 
-        Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames) {
-            $table->unsignedBigInteger('role_id');
+            Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames) {
+                $table->unsignedBigInteger('role_id');
 
-            $table->string('model_type');
-            $table->unsignedBigInteger($columnNames['model_morph_key']);
-            $table->index([$columnNames['model_morph_key'], 'model_type', ]);
+                $table->string('model_type');
+                $table->unsignedBigInteger($columnNames['model_morph_key']);
+                $table->index([$columnNames['model_morph_key'], 'model_type', ]);
 
-            $table->foreign('role_id')
-                ->references('id')
-                ->on($tableNames['roles'])
-                ->onDelete('cascade');
+                $table->foreign('role_id')
+                      ->references('id')
+                      ->on($tableNames['roles'])
+                      ->onDelete('cascade');
 
-            $table->primary(['role_id', $columnNames['model_morph_key'], 'model_type'],
+                $table->primary(['role_id', $columnNames['model_morph_key'], 'model_type'],
                     'model_has_roles_role_model_type_primary');
-        });
+            });
 
-        Schema::create($tableNames['role_has_permissions'], function (Blueprint $table) use ($tableNames) {
-            $table->unsignedBigInteger('permission_id');
-            $table->unsignedBigInteger('role_id');
+            Schema::create($tableNames['role_has_permissions'], function (Blueprint $table) use ($tableNames) {
+                $table->unsignedBigInteger('permission_id');
+                $table->unsignedBigInteger('role_id');
 
-            $table->foreign('permission_id')
-                ->references('id')
-                ->on($tableNames['permissions'])
-                ->onDelete('cascade');
+                $table->foreign('permission_id')
+                      ->references('id')
+                      ->on($tableNames['permissions'])
+                      ->onDelete('cascade');
 
-            $table->foreign('role_id')
-                ->references('id')
-                ->on($tableNames['roles'])
-                ->onDelete('cascade');
+                $table->foreign('role_id')
+                      ->references('id')
+                      ->on($tableNames['roles'])
+                      ->onDelete('cascade');
 
-            $table->primary(['permission_id', 'role_id']);
+                $table->primary(['permission_id', 'role_id']);
 
-            app('cache')->forget('spatie.permission.cache');
-        });
+                app('cache')->forget('spatie.permission.cache');
+            });
+        }
     }
 
     /**
